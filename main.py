c05a50ae0e68684d0b150d79425f02df04ccdf7a
Sergei Yaroschuk <notf8@yandex.ru>
2022-05-24 13:24:45 +0300

    += example

--- main.py
+++ main.py
@@ -3996,3 +3996,25 @@ print(phone_book['Имя'])
 # unic_data_dict = {i_dict['ID']: i_dict for i_dict in data}
 #
 # print(unic_data_dict.values()) # .values() нужен что бы убрать значение ключей из вывода
+
+Задача 1. Песни 2
+запрашивает у пользователя количество песен из списка и названия этих песен, а на экран выводит общее время их звучания
+# violator_songs = {
+#     'World in My Eyes': 4.86,
+#     'Sweetest Perfection': 4.43,
+#     'Personal Jesus': 4.56,
+#     'Halo': 4.9,
+#     'Waiting for the Night': 6.07,
+#     'Enjoy the Silence': 4.20,
+#     'Policy of Truth': 4.76,
+#     'Blue Dress': 4.29,
+#     'Clean': 5.83
+# }
+#
+# songs = int(input('Сколько песен выбрать? '))
+# playlist = dict()
+# for i_song in range(songs):
+#     song = input(f'Название {i_song + 1}-ой песни: ')
+#     playlist[song] = (violator_songs[song])
+#
+# print('Общее время звучания песен:', round(sum(playlist.values()), 2), 'минуты')
