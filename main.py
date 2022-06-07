cb876b8859321038aa65028849e97995a28ac924
Sergei Yaroschuk <notf8@yandex.ru>
2022-06-07 16:18:40 +0300

    += example

--- main.py
+++ main.py
@@ -5145,7 +5145,7 @@ print([{0: 0, 1: 100, 2: 144, 3: 20, 4: 19}[i_key] for i_key in {0: 0, 1: 100, 2
 #
 # print('Результат:')
 # print(my_zip(data1, data2, data3))
-## Есть и третья версия ,с рекрсией, но в ней куча того ,что еще не проходили, типа Try, except, -> и прочих фишек
+## Есть и третья версия ,с рекурсией, но в ней куча того ,что еще не проходили, типа Try, except, -> и прочих фишек
 # def analog_zip_2(*args: iter, count=0, zip_list=[]) -> list[tuple]:
 #     try:
 #         zip_list.append(tuple(list(i)[count] for i in args))
