6722a8c526f5444781cac65dcf994c5c75675bb7
Sergei Yaroschuk <notf8@yandex.ru>
2022-08-14 15:30:17 +0300

    += example

--- OOP_examples.py
+++ OOP_examples.py
@@ -4813,3 +4813,20 @@ print(list(result))
 #
 # print(reduce(check_was, sentences))
 
+Специальная переменная __name__. Если прописать проверку if __name__ == '__main__', то можно запускать отдельные функции и методы из
+импортированного файла.pu иначе будет выполняться весь основной код из импортированного модуя (питон так работает). Эта проверка
+прописывается как в основно (исполняемом) файле, так и в импортированном
+# from functools import reduce
+# def check_was(a, b):
+#     if isinstance(a, str):  # обработаем первый элемент отдельно
+#         a = int(a.count('was'))
+#     result = a + int(b.count('was'))
+#     return result  # т.к. мы возвращаем int - то дальше 'a' всегда будет int-ом, а в 'b' будет новая строка
+#
+# sentences = [
+#     "Nory was a Catholic", "because her mother was a Catholic", "and Nory’s mother was a Catholic",
+#     "because her father was a Catholic", "and her father was a Catholic", "because his mother was a Catholic",
+#     "or had been"
+# ]
+# if __name__ == "__main__":
+#     print(reduce(check_was, sentences))
