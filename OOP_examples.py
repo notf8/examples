accf25356582575988eceab17b9b0d8b295553c8
Sergei Yaroschuk <notf8@yandex.ru>
2022-08-14 13:49:08 +0300

    += example

--- OOP_examples.py
+++ OOP_examples.py
@@ -4799,3 +4799,17 @@ print(list(result))
 # numbers: List[int] = [0, 1, 2, 3, 4]
 #
 # print(reduce(my_add, numbers))
+Используя функцию reduce, реализуйте код, который считает, сколько раз слово was встречается в списке:
+# def check_was(a, b):
+#     if isinstance(a, str):  # обработаем первый элемент отдельно
+#         a = int(a.count('was'))
+#     result = a + int(b.count('was'))
+#     return result  # т.к. мы возвращаем int - то дальше 'a' всегда будет int-ом, а в 'b' будет новая строка
+#
+# sentences = [
+#     "Nory was a Catholic", "because her mother was a Catholic", "and Nory’s mother was a Catholic",
+#     "because her father was a Catholic", "and her father was a Catholic", "because his mother was a Catholic", "or had been"
+# ]
+#
+# print(reduce(check_was, sentences))
+
