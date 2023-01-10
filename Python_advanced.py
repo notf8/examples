edff09768bb5d761200ba066f49ba1ad8b439346
Sergei Yaroschuk <notf8@yandex.ru>
2023-01-10 11:09:59 +0300

    += examlpe

--- Python_advanced.py
+++ Python_advanced.py
@@ -19,3 +19,35 @@
 ========================================================================================================================
 
  - Коды ответов - https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+========================================================================================================================
+
+__________________________________________Полезные функции______________________________________________________________
+
+Точное время через час:
+# import datetime
+#
+# def time_future():
+#     now = datetime.datetime.now()
+#     future = datetime.timedelta(hours=+1)
+#     current_time_after_hour = now + future
+#     return f'«Точное время через час будет {current_time_after_hour.__format__("%H:%M:%S")}»'
+
+Поиск и сбор целых слов в текством файле (без знаков припинания и пробелов, нужно вбить путь к файлу):
+# import re
+# import os
+#
+# def get_word():
+#     data = []
+#     base_dir = os.path.dirname(os.path.abspath(__file__))
+#     book_file = os.path.join(base_dir, 'war_and_peace.txt')
+#     word = re.compile(r'\w*[^.,()!:;"\d\s\-]')
+#     with open(book_file, 'r', encoding='utf-8') as file:
+#         for i in file:
+#             result = re.findall(word, i.strip())
+#             data.extend(result)
+#     return data
+
+Возврат элементов списка через запятую в виде строки (без скобок [])
+# def cars():
+#     car_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+#     return f'Список машин: {", ".join(map(str,[x for x in car_list]))}'
